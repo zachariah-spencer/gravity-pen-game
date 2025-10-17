06ed5609333c0d9fa0024b532ce6763668d34628
zachariah-spencer <spencerzachariah@gmail.com>
2025-10-17 12:35:40 -0500

    Add box rotation and pen with gravity handling

--- app/main.rb
+++ app/main.rb
@@ -1,85 +1,101 @@
 def tick args
-  args.state.logo_rect ||= { x: 576,
-                             y: 200,
-                             w: 128,
-                             h: 101 }
+  defaults args
+  calc args
+  render args
+end
 
-  args.outputs.labels  << { x: 640,
-                            y: 600,
-                            text: 'Hello World!',
-                            size_px: 30,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+def defaults args
+  args.state.pen ||= {
+    path: "sprites/chroma-noir-8x8/items.png",
+    x: 500,
+    y: 500,
+    w: 32,
+    h: 32,
+    tile_x: 8 * 2,
+    tile_w: 8,
+    tile_h: 8,
+    angle: 180,
+    dx: 0.0,
+    dy: 0.0,
+  }
+  
+  args.state.canvas_box_rect ||= {
+    x: Grid.w / 2 - 256,
+    y: Grid.h / 2 - 256,
+    w: 512,
+    h: 512,
+    angle: 0,
+  }
 
-  args.outputs.labels  << { x: 640,
-                            y: 510,
-                            text: "Documentation is located under the ./docs directory. 150+ samples are located under the ./samples directory.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+  args.outputs[:canvas_box].w = 1024
+  args.outputs[:canvas_box].h = 1024
+   
+  args.outputs[:canvas_box].sprites << {
+    x: 256,
+    y: 256,
+    w: 512,
+    h: 512,
+    angle: args.state.canvas_box_rect[:angle],
+    r: 13,
+    g: 13,
+    b: 13,
+  }
+end
 
-  args.outputs.labels  << { x: 640,
-                            y: 480,
-                            text: "You can also access these docs online at docs.dragonruby.org.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+def calc args
+  # inputs
+  args.state.canvas_box_rect[:angle] += 2.0 if args.inputs.keyboard.a
+  args.state.canvas_box_rect[:angle] -= 2.0 if args.inputs.keyboard.d
 
-  args.outputs.labels  << { x: 640,
-                            y: 400,
-                            text: "The code that powers what you're seeing right now is located at ./mygame/app/main.rb.",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+  # base gravity
+  args.state.pen[:dy] -= 0.5
+  
+  args.state.pen[:x] += args.state.pen[:dx]
+  args.state.pen[:y] += args.state.pen[:dy].clamp(-5, 5)
 
-  args.outputs.labels  << { x: 640,
-                            y: 380,
-                            text: "(you can change the code while the app is running and see the updates live)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
+  # collision handling
+  min_x = args.state.canvas_box_rect[:x]
+  max_x = args.state.canvas_box_rect[:x] + args.state.canvas_box_rect[:w] - args.state.pen[:w]
+  min_y = args.state.canvas_box_rect[:y]
+  max_y = args.state.canvas_box_rect[:y] + args.state.canvas_box_rect[:h] - args.state.pen[:h]
 
-  args.outputs.sprites << { x: args.state.logo_rect.x,
-                            y: args.state.logo_rect.y,
-                            w: args.state.logo_rect.w,
-                            h: args.state.logo_rect.h,
-                            path: 'dragonruby.png',
-                            angle: Kernel.tick_count }
-
-  args.outputs.labels  << { x: 640,
-                            y: 180,
-                            text: "(use arrow keys to move the logo around)",
-                            size_px: 20,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
-  args.outputs.labels  << { x: 640,
-                            y: 80,
-                            text: 'Join the Discord Server! https://discord.dragonruby.org',
-                            size_px: 30,
-                            anchor_x: 0.5 }
-
-  if args.inputs.keyboard.left
-    args.state.logo_rect.x -= 10
-  elsif args.inputs.keyboard.right
-    args.state.logo_rect.x += 10
+  if args.state.pen[:x] <= min_x
+    args.state.pen[:x] = min_x
+    args.state.pen[:dx] = 0
+  elsif args.state.pen[:x] > max_x
+    args.state.pen[:x] = max_x
+    args.state.pen[:dx] = 0
   end
 
-  if args.inputs.keyboard.down
-    args.state.logo_rect.y -= 10
-  elsif args.inputs.keyboard.up
-    args.state.logo_rect.y += 10
+  if args.state.pen[:y] <= min_y
+    args.state.pen[:y] = min_y
+    args.state.pen[:dy] = 0
+  elsif args.state.pen[:y] > max_y
+    args.state.pen[:y] = max_y
+    args.state.pen[:dy] = 0
   end
 
-  if args.state.logo_rect.x > 1280
-    args.state.logo_rect.x = 0
-  elsif args.state.logo_rect.x < 0
-    args.state.logo_rect.x = 1280
-  end
+end
 
-  if args.state.logo_rect.y > 720
-    args.state.logo_rect.y = 0
-  elsif args.state.logo_rect.y < 0
-    args.state.logo_rect.y = 720
-  end
+def render args
+  args.outputs.solids << {
+    x: 0,
+    y: 0,
+    w: 1280,
+    h: 720,
+    r: 0,
+    g: 0,
+    b: 0,
+  }
+
+  args.outputs.sprites << {
+    x: Grid.w / 2 - 512,
+    y: Grid.h / 2 - 512,
+    w: 1024,
+    h: 1024,
+    # angle: args.state.canvas_box_rect[:angle],
+    path: :canvas_box
+  }
+
+  args.outputs.sprites << args.state.pen
 end
