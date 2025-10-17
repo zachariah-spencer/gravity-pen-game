bd52a31f0d3a1734542745a53a41994ef15f8c3c
zachariah-spencer <spencerzachariah@gmail.com>
2025-10-17 14:42:33 -0500

    Setup gravity controls and box rotation independently

--- app/main.rb
+++ app/main.rb
@@ -12,9 +12,10 @@ def defaults args
     w: 32,
     h: 32,
     tile_x: 8 * 2,
-    tile_w: 8,
-    tile_h: 8,
-    angle: 180,
+    tile_y: 1,
+    tile_w: 7,
+    tile_h: 7,
+    angle: 180.0,
     dx: 0.0,
     dy: 0.0,
   }
@@ -35,46 +36,94 @@ def defaults args
     y: 256,
     w: 512,
     h: 512,
-    angle: args.state.canvas_box_rect[:angle],
     r: 13,
     g: 13,
     b: 13,
   }
+
+  args.state.gravity_arrow ||= {
+    x: Grid.w * 0.5 - 32,
+    y: Grid.h * 0.5 - 32,
+    w: 64,
+    h: 64,
+    angle: 0,
+    path: "sprites/chroma-noir-8x8/patterns-and-symbols.png",
+    tile_x: 8 * 6,
+    tile_y: 0,
+    tile_w: 8,
+    tile_h: 8
+  }
 end
 
 def calc args
+  # local vars
+  pen = args.state.pen
+  box = args.state.canvas_box_rect
+  arrow = args.state.gravity_arrow
+
+  box_cx = box[:x] + box[:w] * 0.5
+  box_cy = box[:y] + box[:h] * 0.5
+  angle_deg = box[:angle]
+  angle_rad = angle_deg * Math::PI / 180.0
+  cos_a = Math.cos(angle_rad)
+  sin_a = Math.sin(angle_rad)
+
+  arrow_angle_deg = arrow[:angle]
+  arrow_angle_rad = arrow_angle_deg * Math::PI / 180.0
+  arrow_cos_a = Math.cos(arrow_angle_rad)
+  arrow_sin_a = Math.sin(arrow_angle_rad)
+
+  gravity_strength = 0.5
+  max_velocity = 10
+
   # inputs
   args.state.canvas_box_rect[:angle] += 2.0 if args.inputs.keyboard.a
   args.state.canvas_box_rect[:angle] -= 2.0 if args.inputs.keyboard.d
+  args.state.gravity_arrow[:angle] = args.geometry.angle_to([box_cx, box_cy], args.inputs.mouse) + 90 if args.inputs.mouse.button_left
 
-  # base gravity
-  args.state.pen[:dy] -= 0.5
+  # gravity application
+  pen[:dx] += (gravity_strength * arrow_sin_a).clamp(-max_velocity, max_velocity)
+  pen[:dy] -= (gravity_strength * arrow_cos_a).clamp(-max_velocity, max_velocity)
   
-  args.state.pen[:x] += args.state.pen[:dx]
-  args.state.pen[:y] += args.state.pen[:dy].clamp(-5, 5)
+  pen[:x] += pen[:dx].clamp(-max_velocity, max_velocity)
+  pen[:y] += pen[:dy].clamp(-max_velocity, max_velocity)
 
   # collision handling
-  min_x = args.state.canvas_box_rect[:x]
-  max_x = args.state.canvas_box_rect[:x] + args.state.canvas_box_rect[:w] - args.state.pen[:w]
-  min_y = args.state.canvas_box_rect[:y]
-  max_y = args.state.canvas_box_rect[:y] + args.state.canvas_box_rect[:h] - args.state.pen[:h]
+  pen_cx = pen[:x] + pen[:w] * 0.5
+  pen_cy = pen[:y] + pen[:h] * 0.5
 
-  if args.state.pen[:x] <= min_x
-    args.state.pen[:x] = min_x
-    args.state.pen[:dx] = 0
-  elsif args.state.pen[:x] > max_x
-    args.state.pen[:x] = max_x
-    args.state.pen[:dx] = 0
+  local_x = (pen_cx - box_cx) * cos_a + (pen_cy - box_cy) * sin_a
+  local_y = -(pen_cx - box_cx) * sin_a + (pen_cy - box_cy) * cos_a
+  local_dx = pen[:dx] * cos_a + pen[:dy] * sin_a
+  local_dy = -pen[:dx] * sin_a + pen[:dy] * cos_a
+
+  half_w = box[:w] * 0.5 - pen[:w] * 0.5
+  half_h = box[:h] * 0.5 - pen[:h] * 0.5
+
+  if local_x < -half_w
+    local_x = -half_w
+    local_dx = 0
+  elsif local_x > half_w
+    local_x = half_w
+    local_dx = 0
   end
 
-  if args.state.pen[:y] <= min_y
-    args.state.pen[:y] = min_y
-    args.state.pen[:dy] = 0
-  elsif args.state.pen[:y] > max_y
-    args.state.pen[:y] = max_y
-    args.state.pen[:dy] = 0
+  if local_y < -half_h
+    local_y = -half_h
+    local_dy = 0
+  elsif local_y > half_h
+    local_y = half_h
+    local_dy = 0
   end
 
+  world_x = local_x * cos_a - local_y * sin_a + box_cx
+  world_y = local_x * sin_a + local_y * cos_a + box_cy
+
+  pen[:x] = world_x - pen[:w] * 0.5
+  pen[:y] = world_y - pen[:h] * 0.5
+
+  pen[:dx] = local_dx * cos_a - local_dy * sin_a
+  pen[:dy] = local_dx * sin_a + local_dy * cos_a
 end
 
 def render args
@@ -94,8 +143,11 @@ def render args
     w: 1024,
     h: 1024,
     # angle: args.state.canvas_box_rect[:angle],
+    angle: args.state.canvas_box_rect[:angle],
     path: :canvas_box
   }
+
+  args.outputs.sprites << args.state.gravity_arrow
 
   args.outputs.sprites << args.state.pen
 end
